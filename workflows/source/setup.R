1fbabea26a515b8c889560d34162087589e77ac3
Joe Brown <joseph.brown.k@gmail.com>
2024-10-25 11:58:11 -0400

    Editing workflow to avoid errors from mismatched df names etc.

--- workflows/source/setup.R
+++ workflows/source/setup.R
@@ -7,6 +7,13 @@ library(spatstat)
 library(parallel)
 library(ggtern)
 
+# SSP Names
+ssp_names <- c("SSP1-1.9",
+               "SSP1-2.6",
+               "SSP2-4.5",
+               "SSP3-7.0",
+               "SSP5-8.5")
+
 # IPCC values
 ipcc_119_short <- c(0.61, 0.38, 0.85)
 ipcc_119_mid <- c(0.71, 0.40, 1.07)
@@ -28,8 +35,9 @@ ipcc_585_short <- c(0.76, 0.51, 1.04)
 ipcc_585_mid <- c(1.54, 1.08, 2.08)
 ipcc_585_long <- c(3.50, 2.44, 4.82)
 
-ipcc_values <- list(ssp_names[1] = ipcc_119_long,
-                    ssp_names[2] = ipcc_126_long,
-                    ssp_names[3] = ipcc_245_long,
-                    ssp_names[4] = ipcc_370_long,
-                    ssp_names[5] = ipcc_585_long)
+# editing names of IPCC labels
+ipcc_values <- list("SSP1-1.9" = ipcc_119_long,
+                    "SSP1-2.6" = ipcc_126_long,
+                    "SSP2-4.5" = ipcc_245_long,
+                    "SSP3-7.0" = ipcc_370_long,
+                    "SSP5-8.5" = ipcc_585_long)
--- workflows/source/source_all.R
+++ workflows/source/source_all.R
@@ -1,7 +1,8 @@
 ## source all files in `workflows/source` directory
 
+# source("source/names_mapping.R")
+source("source/setup.R")
 source("source/criterion_mapping.R")
-source("source/names_mapping.R")
 source("source/helper_functions.R")
 source("source/criterion_mapping.R")
-source("source/setup.R")
+
